83bc05c4b0486a3b6ef9bfc882f796bd42230525
muhammetozturk95 <mhmozturk7@gmail.com>
2023-12-02 17:51:44 +0300

    db.js guncellendi

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'students',
-  password: 'Muhammet258612',
-  port: 5432,
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'students',
+//   password: 'Muhammet258612',
+//   port: 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'students',
+  password: process.env.DB_PASSWORD || 'Muhammet258612',
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
